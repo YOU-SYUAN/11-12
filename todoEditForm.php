77e9e447f6c3bc0b3e58dab88de1c0aa73291749
LAPTOP-KPICJM2V\ASUS <s107213028@mail1.ncnu.edu.tw>
2020-11-19 17:13:31 +0800

    edit

--- todoEditForm.php
+++ todoEditForm.php
@@ -21,13 +21,20 @@ if (! $rs) {
 <title>無標題文件</title>
 </head>
 <body>
-<h1>Edit Task</h1>
+<h1>貧困學生補助經費申請表</h1>
 <form method="post" action="todoUpdControl.php">
 
 	  <input type='hidden' name='id' value='<?php echo $id ?>'>
       stuid: <input name="stuid" type="text" id="msg" value="<?php echo htmlspecialchars($rs['stuid']);?>" /> <br>
       contact: <input name="contact" type="text" id="msg" value="<?php echo htmlspecialchars($rs['contact']);?>" /> <br>
-	  famstatus: <input name="famstatus" type="text" id="msg" value="<?php echo htmlspecialchars($rs['famstatus']);?>" /> <br>
+	  famstatus: <select  name="famstatus" type="select" id="famstatus" /> 
+				<?php
+					echo "<option value='{$rs['famstatus']}'>{$rs['famtatus']}</option>";
+				?>
+					<option value='低收入戶'>低收入戶</option>
+					<option value='中低收入戶'>中低收入戶</option>
+					<option value='家庭突發因素'>家庭突發因素</option>
+					</select> <br>
 	  content: <input name="content" type="text" id="msg" value="<?php echo htmlspecialchars($rs['content']);?>" /> <br>
 	  status: <input name="status" type="text" id="msg" value="<?php echo htmlspecialchars($rs['status']);?>" /> <br>
 	  
