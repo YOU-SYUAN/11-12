e5e3435d59cc1f1eb0f9c810a4a6afeab0486f03
Walt <s107213022@mail1.ncnu.edu.tw>
2020-11-21 13:58:26 +0800

    增加exresult_SQL

--- student.sql
+++ student.sql
@@ -34,6 +34,7 @@ CREATE TABLE `student` (
   `contact` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `famstatus` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `content` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exresult` text NOT NULL,
   `commit` text CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -42,12 +43,13 @@ CREATE TABLE `student` (
 -- 傾印資料表的資料 `student`
 --
 
-INSERT INTO `student` (`id`, `stuid`, `contact`, `famstatus`, `content`, `commit`, `status`) VALUES
-(1, 'student1', 'm', '低收入戶', '你好', 'commit', 3),
-(2, 'new title', 'job descri', '中低收入戶', 'm', 'commit', 2),
-(3, 'new title', 'job descri', '低收入戶', 'su3cl3a87', 'commit', 1),
-(4, '123456', 'my mother', '家庭突發因素', '我需要這筆錢', '好棒', 0),
-(5, 'myname', 'my mother', '家庭突發因素', '我好可憐', NULL, 0);
+INSERT INTO `student` (`id`, `stuid`, `contact`, `famstatus`, `content`, `exresult`, `commit`, `status`) VALUES
+(1, 'student1', 'm', '低收入戶', '你好', '', 'commit', 3),
+(2, 'new title', 'job descri', '中低收入戶', 'm', '', 'commit', 3),
+(3, 'new title', 'job descri', '低收入戶', 'su3cl3a87', '', 'commit', 1),
+(4, '123456', 'my mother', '家庭突發因素', '他真的需要這筆', '', '好棒', 1),
+(5, 'myname', 'my mother', '家庭突發因素', '我好棒', '1000', '很可以', 0),
+(6, '107213222', 'MR.A', '家庭突發因素', '棒喔', '', '好棒', 0);
 
 --
 -- 已傾印資料表的索引
@@ -67,7 +69,7 @@ ALTER TABLE `student`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
